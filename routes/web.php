f8032952dabef2b78f63c5532e44275e0bad58df
jose2w <jose2001xdh@gmail.com>
2024-10-12 20:33:38 -0500

    Migraciones

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\PrimerControlador;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view ('welcome');
+});
+
+Route::get('/test',[PrimerControlador::class,'index']);
+
+Route::get('otro/{post}/{otro}',[PrimerControlador::class,'otro']);
